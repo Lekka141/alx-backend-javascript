b6dc78e3cd1a639cdf10ae61b080f8963c92a1e8
Keegan <keeganjoubert@protonmail.com>
2024-09-20 23:23:41 +0200

    Eslint

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,5 +1,4 @@
 import fs from 'fs';
-
 /**
  * Reads the data of students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
@@ -43,6 +42,5 @@ const readDatabase = (dataPath) => new Promise((resolve, reject) => {
     });
   }
 });
-
 export default readDatabase;
 module.exports = readDatabase;
