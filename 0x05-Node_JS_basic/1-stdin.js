cda3c85f59edab38cfd3d374cba5e5e18758ebfe
Keegan <keeganjoubert@protonmail.com>
2024-09-20 22:21:02 +0200

    Eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,18 @@
+/** Prompt user for their name */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+/** Handle readable events on stdin */
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
-
+  
   if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
+    /** Trim input to remove extra newlines or spaces */
+    const name = chunk.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
   }
 });
 
+/** Handle end events on stdin */
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
